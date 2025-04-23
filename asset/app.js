df32a7ea0c9ecfdf241f2f6470de89231ecab6d2
amreditor <adammuhammadridwan123@gmail.com>
2025-04-23 15:25:15 +0700

    commit

---
+++ asset/app.js
@@ -0,0 +1,6 @@
+let links = document.querySelector('.nav-links');
+let togglen = document.querySelector('.tombol');
+
+togglen.addEventListener('click', ()=>{
+    links.classList.toggle('show')
+})
